efbfc861dded272290953ed2519e75b30f65d2fb
Niket Totade <tniketsuryabhan40@gmail.com>
2021-04-24 15:07:35 +0530

    created pages and configured navigation links

--- car_service/urls.py
+++ car_service/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('contact/', views.contact, name='contact')
 ]
+
