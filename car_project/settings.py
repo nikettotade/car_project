f95135a9d2c18121fecd58d8b9daebefba825754
Niket Totade <tniketsuryabhan40@gmail.com>
2021-04-24 18:20:11 +0530

    completed teams model

--- car_project/settings.py
+++ car_project/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'car_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
     }
 }
 
@@ -124,3 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'car_project/static'),
 ]
+
+# Media Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
